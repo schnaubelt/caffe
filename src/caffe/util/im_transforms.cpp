122540c566591de9f63fb8af5c95c98b03f0155e
Q-engineering <info@qengineering.eu>
2020-08-20 14:49:27 +0200

    redefine opencv 4 #defines in im_transforms.cpp

--- src/caffe/util/im_transforms.cpp
+++ src/caffe/util/im_transforms.cpp
@@ -1,11 +1,14 @@
 #ifdef USE_OPENCV
 #include <opencv2/highgui/highgui.hpp>
 
-#if CV_VERSION_MAJOR == 3
+#if CV_VERSION_MAJOR == 4
 #include <opencv2/imgcodecs/imgcodecs.hpp>
 #define CV_GRAY2BGR cv::COLOR_GRAY2BGR
 #define CV_BGR2GRAY cv::COLOR_BGR2GRAY
 #define CV_BGR2YCrCb cv::COLOR_BGR2YCrCb
+#define CV_BGR2HSV cv::COLOR_BGR2HSV
+#define CV_HSV2BGR cv::COLOR_HSV2BGR
+#define CV_BGR2Lab cv::COLOR_BGR2Lab
 #define CV_YCrCb2BGR cv::COLOR_YCrCb2BGR
 #define CV_IMWRITE_JPEG_QUALITY cv::IMWRITE_JPEG_QUALITY
 #define CV_LOAD_IMAGE_COLOR cv::IMREAD_COLOR
